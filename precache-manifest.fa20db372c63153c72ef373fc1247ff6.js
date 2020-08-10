8200fb7c3235e2b10c91b010e4f1a9139f06bf25
Ane Diaz de Tuesta <anediaz@gmail.com>
2020-08-10 23:03:38 +0200

    Updates

---
+++ precache-manifest.fa20db372c63153c72ef373fc1247ff6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b69a7bfad142f7bbdf30081cd2fbc86",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "d45f412b7e7f53c4572b",
+    "url": "/my-website/static/css/main.db7f671e.chunk.css"
+  },
+  {
+    "revision": "9b85e77d6b564e514031",
+    "url": "/my-website/static/js/2.1d0ad7fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-website/static/js/2.1d0ad7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45f412b7e7f53c4572b",
+    "url": "/my-website/static/js/main.d4ca0d79.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "7c8c5893bbb9985355941141632f7357",
+    "url": "/my-website/static/media/ane.7c8c5893.png"
+  },
+  {
+    "revision": "db31f34a328d758826e6ae944e918899",
+    "url": "/my-website/static/media/ane2.db31f34a.png"
+  },
+  {
+    "revision": "7ed3ee85f19a8013b3d651c4f1567ccd",
+    "url": "/my-website/static/media/title-white.7ed3ee85.svg"
+  }
+]);
