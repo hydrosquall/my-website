9423f3799867e5971394a6381c9c7c5811a3c161
Ane Diaz de Tuesta <anediaz@gmail.com>
2021-04-11 13:30:47 +0200

    [FEAT] ImageWithLoader: create new component and add it to index

---
+++ src/components/ImageWithLoader/ImageWithLoader.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './ImageWithLoader.css';
+import * as PropTypes from 'prop-types';
+
+const ImageWithLoader = ({cls, image, alt, loader, url, onClick = () => {}}) => {
+  const [loading, setLoading] = useState(true);
+  const img = <img src={image} className={!loading ? 'loaded' : ''} alt={alt} onLoad={() => setLoading(false)} onClick={onClick}/>
+  return(
+    <div className={`ImageWithLoader ${cls || ''}`}>
+      {loading && loader}
+      {url ?
+        <a href={url} target="_blank" rel="noopener noreferrer">{img}</a>
+      : img}
+    </div>
+  )
+}
+
+ImageWithLoader.propTypes = {
+  cls: PropTypes.string,
+  image: PropTypes.node.isRequired,
+  alt: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  loader: PropTypes.node.isRequired,
+  onClick: PropTypes.func
+}
+
+export default ImageWithLoader;
--- src/components/index.js
+++ src/components/index.js
@@ -7,5 +7,7 @@ export { default as LastNews } from './LastNews/LastNews';
 export { default as Menu } from './Menu/Menu';
 export { default as Section } from './Section/Section';
 export { default as Article } from './Article/Article';
+export { default as Lightbox} from './Lightbox/Lightbox';
 export { default as LoaderCircle } from './Loader/LoaderCircle';
 export { default as LoaderInline } from './Loader/LoaderInline';
+export { default as ImageWithLoader } from './ImageWithLoader/ImageWithLoader';
