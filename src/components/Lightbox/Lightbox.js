7a1a8bddb105d007f3d8040ae09fd9054a8f92f4
Ane Diaz de Tuesta <anediaz@gmail.com>
2021-04-11 13:32:47 +0200

    [FEAT] Lightbox: use ImageWithLoader component to display image

--- src/components/Lightbox/Lightbox.js
+++ src/components/Lightbox/Lightbox.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import { ImageWithLoader, LoaderInline } from '../';
 import './Lightbox.css'
 
 const Lightbox = ({src, onClick}) => {
@@ -18,7 +19,7 @@ const Lightbox = ({src, onClick}) => {
   return (<div className="lightbox">
     <div className="lightbox-layer"/>
     <div className="close"><i className="fa fa-times-circle" aria-hidden="true" onClick={onClick} title="close"></i></div>
-    <img src={src} alt="lightbox"/>
+    <ImageWithLoader alt="lightbox" image={src} loader={<LoaderInline height="80" width="80" />}/>
   </div>)
 }
 
